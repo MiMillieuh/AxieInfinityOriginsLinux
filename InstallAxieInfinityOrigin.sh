1cf37c933871c73a74aaf70a19cdeeb0aaf030ba
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2022-12-17 16:32:52 +0100

    Autoupdater Test

--- InstallAxieInfinityOrigin.sh
+++ InstallAxieInfinityOrigin.sh
@@ -43,10 +43,35 @@ tar -xf /tmp/AxieInfinity-Origins.tar.xz -C /tmp/
 
 cp -r /tmp/Axie\ Infinity\ -\ Origins $instdir/AxieInfinity/drive_c/Program\ Files/
 rm -rf /tmp/Axie\ Infinity\ -\ Origins 
+rm -rf /tmp/AxieInfinity-Origins.tar.xz
 
 touch $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'echo "none" > '$instdir'/AxieInfinity/drive_c/axieverserver.mimidb' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'echo "none" > curl -L lulucloud.mywire.org/FileHosting/GithubProjects/AxieInfinityOrigin/axiever.mimidb -o '$instdir'/AxieInfinity/drive_c/axieverserver.mimidb' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'LOCAL=$(cat "'$instdir'/AxieInfinity/drive_c/axieverlocal.mimidb")' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'SERVER=$(cat "'$instdir'/AxieInfinity/drive_c/axieverserver.mimidb")' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'if [[ $LOCAL = $SERVER ]] then' >> $instdir/AxieInfinity/drive_c/launcher.sh
 echo 'export WINEPREFIX="'$instdir'/AxieInfinity"' >> $instdir/AxieInfinity/drive_c/launcher.sh
 echo 'WINEPREFIX='$instdir'/AxieInfinity DXVK_LOG_PATH='$instdir'/AxieInfinity DXVK_STATE_CACHE_PATH='$instdir'/AxieInfinity wine' $instdir'/AxieInfinity/drive_c/Program\ Files/Axie\ Infinity\ -\ Origins/AxieInfinity-Origins.exe $FILE_PATH' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'else if [[ $SERVER = "none" ]] then' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'echo "offline"' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'export WINEPREFIX="'$instdir'/AxieInfinity"' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'WINEPREFIX='$instdir'/AxieInfinity DXVK_LOG_PATH='$instdir'/AxieInfinity DXVK_STATE_CACHE_PATH='$instdir'/AxieInfinity wine' $instdir'/AxieInfinity/drive_c/Program\ Files/Axie\ Infinity\ -\ Origins/AxieInfinity-Origins.exe $FILE_PATH' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'else' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'zenity --info --text "An update is availible, your game will launch just after it..." &' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'rm -r '$instdir'/AxieInfinity/drive_c/Program\ Files/Axie\ Infinity\ -\ Origins' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'echo "Downloading Axie Infinity Origins..."' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'wget https://lulucloud.mywire.org/FileHosting/GithubProjects/AxieInfinityOrigin/AxieInfinity-Origins.tar.xz -O /tmp/AxieInfinity-Origins.tar.xz' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'tar -xf /tmp/AxieInfinity-Origins.tar.xz -C /tmp/' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'rm -rf /tmp/AxieInfinity-Origins.tar.xz' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'rm '$instdir'/AxieInfinity/drive_c/axieverlocal.mimidb' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'mv '$instdir'/AxieInfinity/drive_c/axieverserver.mimidb '$instdir'/AxieInfinity/drive_c/axieverlocal.mimidb' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'sleep 3' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'export WINEPREFIX="'$instdir'/AxieInfinity"' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'WINEPREFIX='$instdir'/AxieInfinity DXVK_LOG_PATH='$instdir'/AxieInfinity DXVK_STATE_CACHE_PATH='$instdir'/AxieInfinity wine' $instdir'/AxieInfinity/drive_c/Program\ Files/Axie\ Infinity\ -\ Origins/AxieInfinity-Origins.exe $FILE_PATH' >> $instdir/AxieInfinity/drive_c/launcher.sh
+echo 'fi fi' >> $instdir/AxieInfinity/drive_c/launcher.sh
+
+
 
 chmod +x $instdir/AxieInfinity/drive_c/launcher.sh
 
